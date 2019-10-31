247ddc540f3d76c21713c4a28741eb11eb462ca4
sjeremich23 <51142646+sjeremich23@users.noreply.github.com>
2019-10-31 05:17:49 -0600

    darkMode change

--- src/hooks/useDarkMode.jsx
+++ src/hooks/useDarkMode.jsx
@@ -2,7 +2,7 @@ import { useLocalStorage } from "./useLocalStorage";
 
 export const useDarkMode = value => {
   const [darkMode, setDarkMode] = useLocalStorage(value);
-  !darkMode
+  darkMode
     ? document.body.classList.add("dark-mode")
     : document.body.classList.remove("dark-mode");
   return [darkMode, setDarkMode];
